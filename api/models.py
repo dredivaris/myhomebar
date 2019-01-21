034f1c876f52ebfdc423862931b50ca48dbfffb9
andreas divaris <dredivaris@gmail.com>
2019-01-20 16:22:15 -0800

    Add lenght limiters to models

--- api/models.py
+++ api/models.py
@@ -3,21 +3,22 @@ from django.db import models
 
 
 class Ingredient(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=1000)
 
 
 class Beverage(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=1000)
     owner = models.ForeignKey(User)
     ingredients = models.ManyToManyField(Ingredient, through='BeverageIngredients')
 
 
 class Unit(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=200)
 
 
 class Quantity(models.Model):
-    amount = models.DecimalField()
+    amount = models.DecimalField(decimal_places=2, max_digits=3)
+    divisor = models.IntegerField(blank=True)
     unit = models.ForeignKey(Unit)
 
 
