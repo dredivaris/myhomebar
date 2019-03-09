9b28ca0e74dace19253f7607f4d726badfc8e602
andreas divaris <dredivaris@gmail.com>
2019-03-09 12:36:38 -0800

    Update data model with additional beverage fields

--- api/models.py
+++ api/models.py
@@ -21,12 +21,15 @@ class Beverage(models.Model):
     ingredients = models.ManyToManyField(Ingredient, through='BeverageIngredients')
     description = models.TextField(null=True)
     directions = models.TextField(null=True)
+    attribution = models.TextField(null=True)
+    glassware = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return self.name
 
 
 class Unit(models.Model):
+    # TODO: is this how we want to handle units?
     name = models.CharField(max_length=200)
 
     def __str__(self):
