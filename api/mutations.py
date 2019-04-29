1d095afc9f9a8b7c440499179d67ff088db28f3b
andreas divaris <dredivaris@gmail.com>
2019-04-29 16:25:12 -0700

    Add user creation endpoint

--- api/mutations.py
+++ api/mutations.py
@@ -4,6 +4,38 @@ from graphql import GraphQLError
 from api.types import AddRecipeResponseGraphql
 from api.validators import RecipeValidator
 
+from django.contrib.auth import get_user_model
+
+from graphene_django import DjangoObjectType
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = get_user_model()
+
+
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+        first_name = graphene.String()
+        last_name = graphene.String()
+
+    def mutate(self, info, username, password, email, first_name='', last_name=''):
+        user = get_user_model()(
+            username=username,
+            email=email,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.set_password(password)
+        user.save()
+
+        return CreateUser(user=user)
+
 
 class AddRecipe(graphene.Mutation):
     class Arguments:
@@ -31,5 +63,7 @@ class AddRecipe(graphene.Mutation):
         recipe_validator.validate()
         recipe_validator.save()
 
+
 class Mutation(object):
     add_recipe = AddRecipe.Field()
+    create_user = CreateUser.Field()
