245a8b7d95c32fbef4e116f40a6c3d94129c7072
andreas divaris <dredivaris@gmail.com>
2019-07-13 12:13:52 -0700

    Latest iteration of auth

--- api/queries.py
+++ api/queries.py
@@ -1,8 +1,10 @@
 import graphene
+from django.contrib.auth import get_user_model
 
 from graphene_django.types import DjangoObjectType
 
-from api.models import Ingredient
+from api.models import Ingredient, Recipe
+from api.mutations import UserType
 
 
 class IngredientType(DjangoObjectType):
@@ -10,8 +12,35 @@ class IngredientType(DjangoObjectType):
         model = Ingredient
 
 
+class RecipeType(DjangoObjectType):
+    class Meta:
+        model = Recipe
+
+
 class Query(object):
     all_ingredients = graphene.List(IngredientType)
 
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.all()
+
+    users = graphene.List(UserType)
+
+    def resolve_users(self, info):
+        return get_user_model().objects.all()
+
+    me = graphene.Field(UserType)
+
+    def resolve_me(self, info):
+
+        user = info.context.user
+        # import pdb
+        # pdb.set_trace()
+        if user.is_anonymous:
+            raise Exception('Not logged in!')
+
+        return user
+
+    users_recipes = graphene.List(RecipeType)
+
+    def resolve_users_recipes(self, info, user_id):
+        return self.users_recipes
