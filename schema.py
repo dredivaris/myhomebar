0de84cd5ba83f61a83546a435da58e4da913c4d9
andreas divaris <dredivaris@gmail.com>
2019-09-21 16:06:14 -0700

    Add token login mutation

--- schema.py
+++ schema.py
@@ -1,12 +1,23 @@
 import graphene
+import graphql_jwt
 
-import api.schema
+
+import api.queries
+import api.mutations
 
 
-class Query(api.schema.Query, graphene.ObjectType):
+class Query(api.queries.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(api.mutations.Mutation, graphene.ObjectType):
+    # This class will inherit from multiple Queries
+    # as we begin to add more apps to our project
+    token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+    verify_token = graphql_jwt.Verify.Field()
+    refresh_token = graphql_jwt.Refresh.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
