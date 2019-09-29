5e08df57c44e0107594800cfe054f3febb6b9773
andreas divaris <dredivaris@gmail.com>
2019-09-28 17:47:08 -0700

    Add recipes getter and optimize query

--- api/queries.py
+++ api/queries.py
@@ -1,9 +1,10 @@
 import graphene
 from django.contrib.auth import get_user_model
+from django.db.models import Prefetch
 
 from graphene_django.types import DjangoObjectType
 
-from api.models import Ingredient, Recipe
+from api.models import Ingredient, Recipe, RecipeIngredient
 from api.mutations import UserType
 
 
@@ -12,9 +13,19 @@ class IngredientType(DjangoObjectType):
         model = Ingredient
 
 
+class RecipeIngredientType(DjangoObjectType):
+    class Meta:
+        model = RecipeIngredient
+
+
 class RecipeType(DjangoObjectType):
     class Meta:
         model = Recipe
+
+    ingredients = graphene.List(graphene.String)
+
+    def resolve_ingredients(self, info):
+        return self.ingredients_set
 
 
 class Query(object):
@@ -44,8 +55,14 @@ class Query(object):
         if user.is_anonymous:
             raise Exception('Not logged in!')
 
-        recipes = Recipe.objects.filter(owner=user)
-        print(f'recipes for user {user} are {recipes}, len tot {Recipe.objects.count()}')
+        ri_query = RecipeIngredient.objects.select_related('ingredient', 'quantity__unit')
+        recipes = Recipe.objects.filter(
+            owner=user,
+            recipe_type=Recipe.COCKTAIL
+        ).prefetch_related(
+            Prefetch('recipeingredient_set', queryset=ri_query, to_attr='ingredients_set')
+        )
+        # print(f'recipes for user {user} are {recipes}, len tot {Recipe.objects.count()}')
         return recipes
 
     users_recipe = graphene.Field(RecipeType, id=graphene.Int(required=True))
