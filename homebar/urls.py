4717305dd568eaf783c30b566e98ae0ef5770c45
andreas divaris <dredivaris@gmail.com>
2019-10-06 15:34:53 -0700

    Update with graphql url

--- homebar/urls.py
+++ homebar/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from graphene_django.views import GraphQLView
 
@@ -22,5 +23,5 @@ from schema import schema
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
