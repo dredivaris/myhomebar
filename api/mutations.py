b023c1b73e7da1b488c0204ebf25183c57d4c743
andreas divaris <dredivaris@gmail.com>
2020-09-17 18:04:02 -0700

    Add recipe and pantry mutations

--- api/mutations.py
+++ api/mutations.py
@@ -1,8 +1,11 @@
 import graphene
 from graphql import GraphQLError
 
-from api.domain import create_recipe
-from api.types import AddRecipeResponseGraphql
+from api.domain import create_recipe, create_recipe_from_plaintext, save_recipe_from_parsed_recipes, \
+    tokenize_recipes_from_plaintext
+from api.models import Pantry, Ingredient, PantryIngredient
+from api.types import AddRecipeResponseGraphql, AddRecipesFromTextResponseGraphql, \
+    LinkOrCreateIngredientsForPantryResponseGraphql
 
 from django.contrib.auth import get_user_model
 
@@ -33,6 +36,7 @@ class CreateUser(graphene.Mutation):
         )
         user.set_password(password)
         user.save()
+        Pantry.objects.create(owner=user)
 
         return CreateUser(user=user)
 
@@ -63,6 +67,80 @@ class AddRecipe(graphene.Mutation):
         create_recipe(args, info.context.user)
 
 
+class AddRecipesFromText(graphene.Mutation):
+    class Arguments:
+        recipe_text = graphene.String(required=True)
+
+    Output = AddRecipesFromTextResponseGraphql
+
+    def mutate(self, info, recipe_text):
+        print(f'text received: {recipe_text}')
+        recipes = tokenize_recipes_from_plaintext(recipe_text)
+        print(recipes)
+        recipes_saved_ids = []
+        for recipe in recipes:
+            # TODO: validate not an existing recipe
+            recipes_saved_ids.append(save_recipe_from_parsed_recipes(recipe))
+
+        return AddRecipesFromTextResponseGraphql(recipe_ids=recipes_saved_ids)
+
+
+class LinkOrCreateIngredientsForPantry(graphene.Mutation):
+    class Arguments:
+        ingredients = graphene.List(graphene.String, required=True)
+
+    Output = LinkOrCreateIngredientsForPantryResponseGraphql
+
+    def mutate(self, info, ingredients):
+        print(f'text received: {ingredients}')
+        existing_ingredient_ids, new_ingredient_texts = [], []
+
+        pantry = Pantry.objects.get(owner_id=1)  # TODO: implement proper login!
+        for id_or_new_ingredient in ingredients:
+            if id_or_new_ingredient.isnumeric():
+                existing_ingredient_ids.append(int(id_or_new_ingredient))
+            else:
+                new_ingredient_texts.append(id_or_new_ingredient)
+
+        new_ingredient_ids = []
+        if new_ingredient_texts:
+            for new_ingredient in new_ingredient_texts:
+                ingredient = Ingredient.objects.create(
+                    name=new_ingredient,
+                    owner_id=1,  # TODO: implement proper login!
+                    is_garnish=False
+                )
+                new_ingredient_ids.append(ingredient.id)
+
+        ids_to_add = new_ingredient_ids + existing_ingredient_ids
+
+        PantryIngredient.objects.bulk_create([
+            PantryIngredient(pantry=pantry, ingredient_id=i) for i in ids_to_add
+        ])
+
+        return LinkOrCreateIngredientsForPantryResponseGraphql(ingredient_ids=ids_to_add)
+
+
+'''
+web        | {'attribution': 'Jimbo',
+web        |  'directions': 'one \ntwo\nthree',
+web        |  'garnish': 'flower',
+web        |  'glassware': 'coupe',
+web        |  'ingredients': 'one\ntwo and \nthree',
+web        |  'name': 'dre',
+web        |  'notes': 'afasf',
+web        |  'rating': 0.0,
+web        |  'rating_set': False,
+web        |  'recipe_type': 'COCKTAIL',
+web        |  'source': 'rad',
+web        |  'source_url': '',
+web        |  'tools': 'grinder'}
+'''
+
+
+
 class Mutation(object):
     add_recipe = AddRecipe.Field()
     create_user = CreateUser.Field()
+    add_recipes_from_text = AddRecipesFromText.Field()
+    link_or_create_ingredients_for_pantry = LinkOrCreateIngredientsForPantry.Field()
