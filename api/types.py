39168946c7ea573d83e920f4bd5a5f4db320f2bd
andreas divaris <dredivaris@gmail.com>
2020-10-11 12:49:11 -0700

    Add some return types

--- api/types.py
+++ api/types.py
@@ -3,3 +3,31 @@ import graphene
 
 class AddRecipeResponseGraphql(graphene.ObjectType):
     recipe_id = graphene.Int()
+
+
+class AddRecipesFromTextResponseGraphql(graphene.ObjectType):
+    recipe_ids = graphene.List(graphene.Int)
+
+
+class LinkOrCreateIngredientsForPantryResponseGraphql(graphene.ObjectType):
+    ingredient_ids = graphene.List(graphene.Int)
+
+
+class AddRecipeFlexibleResponseGraphql(graphene.ObjectType):
+    recipe_id = graphene.Int()
+
+
+class IngredientBulkUpdateResponseGraphql(graphene.ObjectType):
+    count = graphene.Int()
+
+
+class IngredientCreateResponseGraphql(graphene.ObjectType):
+    created = graphene.Boolean()
+
+
+class ToggleStockResponseGraphql(graphene.ObjectType):
+    toggled_to = graphene.Boolean()
+
+
+class EditIngredientFlexibleResponseGraphql(graphene.ObjectType):
+    added = graphene.Boolean()
